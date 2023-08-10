341d2e5eb524974aa3c6d608de26716d36eb210e
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2023-08-09 19:50:30 -0700

    Updated ClientNotification.py

--- PC Files/ClientNotification.py
+++ PC Files/ClientNotification.py
@@ -1,7 +1,7 @@
 import socket
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect(("192.168.1.25", 5000))
+socket.connect(("PiName / IP Address", 5000))
 
 while True:
         msg = socket.recv(1024).decode("utf-8")
